f18fd488de6b856643251a292dcaa7e0e928b1e0
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-23 23:58:55 -0600

    Updates with changes for Challenges, ISS Spotter 2-4

---
+++ iss.js
@@ -0,0 +1,81 @@
+const request = require('request');
+
+const fetchMyIP = (callback) => {
+  const url = "https://api.ipify.org?format=json";
+
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+
+    const data = JSON.parse(body);
+    callback(null, data.ip);
+  });
+};
+
+const fetchCoordsByIP = (ip, callback) => {
+  const url = "https://freegeoip.app/json/";
+
+  request(url + ip, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+
+const fetchISSFlyOverTimes = (coords, callback) => {
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching fly over times. Response: ${response}`;
+      return callback(Error(msg), null);
+    }
+
+    const data = JSON.parse(body);
+    callback(null, data.response);
+  });
+};
+
+const nextISSTimesForMyLocation = function(callback) {
+  
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+
+
+module.exports = { nextISSTimesForMyLocation };
